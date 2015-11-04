d1889837549b4ce5f1bc8111f49adaf054c24f30
Nathaniel <nathanielrhoda@gmail.com>
2015-11-04 10:32:31 +0200

    final app

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  resources :genres
+
+  resource :session
+  
+  get 'signup' => 'users#new'
+  
+  resources :users
+
+  root "movies#index"
+
+  # get "movies/filter/hits" => "movies#index", scope: "hits"
+  # get "movies/filter/flops" => "movies#index", scope: "flops"
+  get 'movies/filter/:scope' => "movies#index", as: :filtered_movies
+  resources :movies do
+    resources :reviews
+    resources :favorites
+  end
+end
